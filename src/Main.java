7225effd80750e2655503a27eb729afb707383b8
Yuliya Shkuratko <sm0ci@mail.ru>
2021-10-24 00:58:33 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 2_000_000_000;
+        int addition = 500_000_000;
+        int total = count + addition;
+        System.out.println(total);
+    }
+}
